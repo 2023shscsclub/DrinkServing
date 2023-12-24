413af9d736289d939865b7806ca49c90a0852f70
DOHYUNN <vinke1717@gmail.com>
2023-12-24 21:08:41 +0800

    Display Feature Add

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import Arm_Lib
 import time
 import requests
 import pyfirmata
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 board = pyfirmata.Arduino('/dev/ttyUSB0')
 
@@ -61,9 +66,39 @@ def drink_serve(drink_type):
 
 if __name__ == "__main__":
     while True:
+        try:
+            RST = None
+            disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=1, gpio=1)
+            disp.begin()
+            disp.clear()
+            disp.display()
+            width = disp.width
+            height = disp.height
+            image = Image.new('1', (width, height))
+            draw = ImageDraw.Draw(image)
+            draw.rectangle((0,0,width,height), outline=0, fill=0)
+            padding = -2
+            top = padding
+            bottom = height-padding
+            x = 0
+            font = 'malgun.ttf'
+            try:
+                font = ImageFont.truetype(font, 9)
+            except:
+                font = ImageFont.load_default()
+        except :
+            time.sleep(1)
+            continue
+        break
+    while True:
         orders = requests.get("https://mbp16.ez0.us/csclub/orders/").json()
         print("Orders: " + str(orders))
         for i in orders:
+            draw.rectangle((0,0,width,height), outline=0, fill=0)
+            draw.text((x, top), "Name: " + i["name"], font=font, fill=255)
+            draw.text((x, top + 15), "Phone: " + i["phone"], font=font, fill=255)
+            disp.image(image)
+            disp.display()
             drink_serve(int(i["drink"]))
             requests.delete("https://mbp16.ez0.us/csclub/orders/", json={"name": i["name"]})
         print(str(server_check_interval) + " Seconds Starting")
